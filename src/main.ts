eba8121e918f9d8dd99a58ac9d2d928129a4bdcd
Dandy Candra <dandycandrapratama@gmail.com>
2023-08-11 23:01:14 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); //Global Validation Pipes
-  await app.listen(8000);
+  await app.listen(3000);
 }
 
 bootstrap();
